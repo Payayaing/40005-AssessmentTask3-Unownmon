61edd6a978b8c93dc027f876617025581532e0f6
Payayaing <isabella.watt22@gmail.com>
2025-10-02 23:03:33 +1000

    Added comments and error alerts for remaining views.

--- PokemonWordle/PokemonWordle/View/PokemonDetailView.swift
+++ PokemonWordle/PokemonWordle/View/PokemonDetailView.swift
@@ -9,15 +9,18 @@ import SwiftUI
 import Combine
 
 struct PokemonDetailView: View {
+    // This view displays detailed stats for each Pokemon, requiring the API number (or list index) to fetch the data correctly.
     @State var index: Int
     @State var pokemon: Pokemon?
     @State var showSheet: Bool = false
+    @State var showAlert: Bool = false
     @EnvironmentObject var gameViewModel: GameViewModel
     @Environment(\.dismiss) var dismiss
     @Binding var path: NavigationPath
     
     var body: some View {
         VStack {
+            // Safely unwraps the pokemon to avoid crashes from forcefully unwrapping a nil value, as the application needs to wait for the PokeAPI fetch. Before the data is successfully obtained, the Pokemon is nil.
             if let pokemon = pokemon {
                 Spacer()
                 HStack {
@@ -33,6 +36,7 @@ struct PokemonDetailView: View {
                             .font(.title)
                             .bold()
                         
+                        // Display each type as text with corresponding type colours.
                         HStack {
                             ForEach(pokemon.pokemonData.types, id: \.name) { type in
                                 Text(type.format())
@@ -47,6 +51,7 @@ struct PokemonDetailView: View {
                     }
                 }
                 
+                // Show height and weight on a linear scale for a more intuitive user experience. The minimum and maximum labels correspond to the minimum and maximum bounds for height and weight respectively.
                 Gauge(value: pokemon.getHeight(), in: 0.1...20.0) {
                     Text("Height (m)")
                         .font(.headline)
@@ -58,7 +63,7 @@ struct PokemonDetailView: View {
                 } maximumValueLabel: {
                     Text("20.0")
                 }
-                .tint(pokemon.pokemonData.types.first!.color())
+                .tint(pokemon.pokemonData.types.first!.color()) // Pokemon always have two types, so this is guaranteed to be safe.
                 .padding()
             
                 Gauge(value: pokemon.getWeight(), in: 0.1...999.9) {
@@ -82,6 +87,7 @@ struct PokemonDetailView: View {
                 
                 VStack {
                     Button(action: {
+                        // Notes are shown as a sheet attached to this view. This triggers the sheet to appear.
                         self.showSheet = true
                     }) {
                         HStack {
@@ -96,6 +102,7 @@ struct PokemonDetailView: View {
                     }
                     
                     Button(action: {
+                        // Adds the Pokemon to the guesses, and navigates the user back to GameView.
                         gameViewModel.addPokemon(pokemon: pokemon)
                         path.removeLast(2)
                     }) {
@@ -112,21 +119,32 @@ struct PokemonDetailView: View {
                 }
                 .padding()
             } else {
+                // While the Pokemon is nil, display 'Loading'.
                 Spacer()
                 Text("Loading...")
                 Spacer()
             }
         }
         .onAppear(perform: {
+            // When the view appears, fetch the selected Pokemon's data to display. If this process fails, then alert the user and provides instructions for what the user should do.
             Task {
                 if let data = await gameViewModel.fetchData(index: index) {
                     self.pokemon = Pokemon(pokemonData: data)
+                } else {
+                    self.showAlert = true
                 }
             }
         })
         .sheet(isPresented: $showSheet) {
             NoteView()
                 .presentationDetents([.medium, .large])
+        }
+        .alert("An Error Occurred", isPresented: $showAlert) {
+            Button("OK", role: .cancel) {
+                dismiss()
+            }
+        } message: {
+            Text("An error occurred trying to fetch the data for this Pokemon. Please try again in a little bit or try a different Pokemon. Thank you :>")
         }
     }
 }
--- PokemonWordle/PokemonWordle/View/SelectorView.swift
+++ PokemonWordle/PokemonWordle/View/SelectorView.swift
@@ -9,9 +9,12 @@ import SwiftUI
 import Combine
 
 struct SelectorView: View {
+    // View which displays a full list of 1025 Pokemon that the user can choose from to make their guess. They are able to filter the list using a TextField to search for specific Pokemon, or can scroll through the list manually. They can then tap on a Pokemon to obtained detailed information.
     @StateObject var viewModel = SelectorViewModel()
     @State var isLoading: Bool = true
+    @State var showAlert: Bool = false
     @Binding var path: NavigationPath
+    @Environment(\.dismiss) var dismiss
     
     var body: some View {
         VStack {
@@ -26,6 +29,7 @@ struct SelectorView: View {
                     .padding(.horizontal, 10)
                     .autocorrectionDisabled(true)
                     .onChange(of: viewModel.search) {
+                        // Live list filtering by filtering the list whenever the search query changes.
                         viewModel.filter()
                     }
             }
@@ -39,7 +43,7 @@ struct SelectorView: View {
             } else {
                 List(viewModel.filteredNames, id: \.self) { name in
                     Button(action: {
-                        // The index should always be a valid number. However, in the event that this function does not work as intended, the application should not crash, so show a Bulbasaur instead.
+                        // The index should always be a valid number. However, in the event that this function does not work as intended, the application should not crash, so show a Bulbasaur instead. This shows the selected Pokemon with their detailed stats on a succeeding view.
                         path.append(Screen.pokemonDetail(index: viewModel.getApiNumber(name: name) ?? 1))
                     }) {
                         HStack {
@@ -56,18 +60,30 @@ struct SelectorView: View {
         .padding()
         .onAppear(perform: {
             Task {
+                // Upon view appearing, the application fetches a list of Pokemon names. In the event that this does not work, then trigger an alert to communicate this to the user and provide instructions as to what they should do.
                 if let data = await viewModel.fetchNames() {
                     viewModel.loadNames(names: data)
+                } else {
+                    self.showAlert = true
                 }
                 isLoading = false
             }
         })
+        .alert("An Error Occurred", isPresented: $showAlert) {
+            Button("OK", role: .cancel) {
+                dismiss()
+            }
+        } message: {
+            Text("An error occurred while retrieving the Pokemon list. Please wait a few seconds and try again :>")
+        }
     }
     
+    // Since the full list obtains strings and the sprite requires an index, this converts the name to the corresponding index and returns the relevant link. If somehow an invalid Pokemon is accessed, this should not crash the application.
     private func getURL(name: String) -> URL? {
         guard let index = viewModel.getApiNumber(name: name) else {
             return nil
-            }
+        }
+        // The PokeAPI sprite responses always use this GitHub link. Thus, we can get the image directly from this link instead of having to query the API.
         return URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(index).png")
     }
 }
